688bc7e1a7241e446c960d1ea7fa3afeb2473dd5
Prajwal <iamprajwal77@gmail.com>
2024-02-23 22:14:25 +0530

    new widget

---
+++ lib/widgets/date_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DateBar extends StatelessWidget {
+  const DateBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
